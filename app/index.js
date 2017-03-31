5ad9ca51800692bfa4db34b6ddfe5d227c09a570
liyang <121777824@qq.com>
2017-03-31 20:58:21 +0800

    引入jquery

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 
 import _ from 'lodash'
+import $ from 'jquery'
 
 function component() {
-  var element = document.createElement('div')
+  var element = $('<div></div>')
 
-  element.innerHTML = _.join(['hello','webpack'],' ')
+  element.html(_.join(['hello','webpack'],' '))
 
-  return element  
+  return element.get(0)  
 }
 
 document.body.appendChild(component())
