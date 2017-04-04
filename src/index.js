58464a6b5e2e100f071dd671d16d119579a22fbf
liyang <121777824@qq.com>
2017-04-04 18:06:55 +0800

    build

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div className="wrapper">
+    <div className="content">
+      <div>My First React Demo</div>
+    </div>
+  </div> ,
+  document.getElementById('root')
+);
