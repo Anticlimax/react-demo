5e8df0b1d4dd761b556c069904a3562231e03b25
liyang <121777824@qq.com>
2017-04-11 22:52:26 +0800

    完成基本功能

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
   render(){
-    return <input type="text" value={this.props.content}/>
+    return <input type="text" value={this.props.content}
+                              onKeyPress={this.submit}
+                              onChange={this.changeTitle}/>
+  }
+  submit=(e)=>{
+
+    if(e.key === 'Enter'){
+      this.props.onSubmit(e)
+    }
+  }
+  changeTitle=(e)=>{
+    this.props.onChange(e)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,41 +1,12 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoInput from './TodoInput';
-import TodoItem from './TodoItem'
+import 'normalize.css';
+import './reset.css';
 import './index.css';
+import App from './App'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      newTodo:'test',
-      todoList:[
-        {id:1,title:'第一个待办'},
-        {id:2,title:'第二个待办'}
-      ]
-    }
-  }
 
-  render(){
-    let todos = this.state.todoList.map((item,index)=>{
-      return (
-      <li>
-        <TodoItem todo={item}/>
-      </li>
-      )
-    })
-
-    return (
-      <div className="App">
-        <h1>我的待办</h1>
-        <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
-        </div>
-        <ol>
-          {todos}
-        </ol>
-      </div>
-    )
-  }
-}
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
